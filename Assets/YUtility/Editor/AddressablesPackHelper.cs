df55f8414068b1a749e7c440d4ad86da4a66bd0b
yellowzhong001 <1144916875@qq.com>
2019-11-13 18:51:34 +0800

    资源加载修改

---
+++ Assets/YUtility/Editor/AddressablesPackHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.AddressableAssets;
+using UnityEditor.AddressableAssets.Settings;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class AddressablesPackHelper
+{
+
+    public static void BuildAssetGroup() {
+
+    }
+
+    [MenuItem("YUtil/Test")]
+    public static void BuildAssetPack() {
+        AddressableAssetSettings setting = new AddressableAssetSettings();
+        AddressableAssetGroup group = setting.FindGroup("AssetsGroup");
+        Debug.Log(group.entries);
+    }
+}

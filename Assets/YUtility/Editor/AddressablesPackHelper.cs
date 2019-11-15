ed2731a3c79227737982cc2fc8d976a1d1bcc7d5
yellowzhong001 <1144916875@qq.com>
2019-11-15 15:56:33 +0800

    资源打包操作

--- Assets/YUtility/Editor/AddressablesPackHelper.cs
+++ Assets/YUtility/Editor/AddressablesPackHelper.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.AddressableAssets;
 using UnityEditor.AddressableAssets.Settings;
@@ -8,6 +10,10 @@ using UnityEngine.AddressableAssets;
 
 public class AddressablesPackHelper
 {
+    public static List<string> assetPathStrs;
+    public static List<string> filterStrs;
+
+    public static bool Init = true;
 
     public static void BuildAssetGroup() {
 
@@ -15,8 +21,66 @@ public class AddressablesPackHelper
 
     [MenuItem("YUtil/Test")]
     public static void BuildAssetPack() {
-        AddressableAssetSettings setting = new AddressableAssetSettings();
-        AddressableAssetGroup group = setting.FindGroup("AssetsGroup");
-        Debug.Log(group.entries);
+
+        Init = true;
+
+        AddressableAssetSettings setting = AddressableAssetSettingsDefaultObject.Settings;
+        AddressableAssetGroup group = setting.FindGroup("AssetGroup");
+        List<AddressableAssetEntry> temp = group.entries.ToList();
+        foreach (var item in temp)
+        {
+            setting.RemoveAssetEntry(item.guid);
+        }
+
+        string[] p = AssetDatabase.GetAllAssetPaths();
+        for (int i = 0; i < p.Length; i++)
+        {
+            if (CanPack(p[i])) {
+                setting.CreateOrMoveEntry(AssetDatabase.AssetPathToGUID(p[i]), group);
+            }
+        }
+    }
+
+
+    public static bool CanPack(string path) {
+        if (Init) {
+            if (assetPathStrs == null) {
+                assetPathStrs = new List<string>();
+            }
+            assetPathStrs.Clear();
+
+            if (filterStrs == null)
+            {
+                filterStrs = new List<string>();
+            }
+            filterStrs.Clear();
+
+            assetPathStrs.AddRange(AppSetting.Ins.GetValue("AssetPath").Split(new char[] { ';'},StringSplitOptions.RemoveEmptyEntries));
+
+            filterStrs.AddRange(AppSetting.Ins.GetValue("DontPack").Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries));
+
+            Init = true;
+        }
+
+        bool pack = false;
+        foreach (var item in assetPathStrs)
+        {
+            if (path.StartsWith(item)) {
+                pack = true;
+                break;
+            }
+        }
+
+        if (!pack) { return false; }
+
+        foreach (var item in filterStrs)
+        {
+            if (path.EndsWith(item)) {
+                return false;
+            }
+        }
+
+
+        return true;
     }
 }
